a175e98b94df57d41f2bdb2fbf5d39ad2dc0e7ca
Matthew Hugley <73258263+mhmatthewhugley@users.noreply.github.com>
2021-05-25 11:59:32 -0500

    Modified gtag.js

--- gtag.js
+++ gtag.js
@@ -2,11 +2,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-187215884-2"></script>
 <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag("js", new Date());
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-    gtag("config", "UA-187215884-2");
+  gtag('config', 'UA-187215884-2');
 </script>
