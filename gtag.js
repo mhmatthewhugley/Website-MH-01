59f92f555ceca9ca0243e6fdbf57dd57122b918f
Matthew Hugley <73258263+mhmatthewhugley@users.noreply.github.com>
2021-05-27 23:12:34 -0500

    Updated gtag.js

--- gtag.js
+++ gtag.js
@@ -1,10 +1,10 @@
 // This file is not licensed under any LICENSES specifieid in the NOTICE.txt file
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-187215884-2"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-D9CMZVZQ3B"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-187215884-2');
+  gtag('config', 'G-D9CMZVZQ3B');
 </script>
